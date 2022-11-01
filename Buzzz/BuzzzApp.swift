6d374b542ff20b2c83abe5df08c0f8395d955d4a
Muhammad Ali Rana <alisaleem877@gmail.com>
2022-11-01 15:36:24 +0500

    Initial Commit

---
+++ Buzzz/BuzzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzzApp.swift
+//  Buzzz
+//
+//  Created by MacBook Pro on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BuzzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
